af03917eb8da63539eb89aa422d5bdb2445701ea
Vernon Leadbeater <vernon.leadbeater@gmail.com>
2023-10-05 23:08:45 -0400

    Implemented Dragon Class

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,24 @@
-// Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+
+  static getDragons(...dragons) {
+    return dragons.map(dragon => dragon.name)
+  }
+}
+
+const toothless = new Dragon("Toothless", "black");
+console.log(toothless);
+console.log(toothless.breathesFire());
+
+console.log(Dragon.getDragons(toothless));
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
